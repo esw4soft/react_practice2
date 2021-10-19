bbf40f5c20aab759731c8b3b3454c57b86c8a4c1
easonw <es250528@gmail.com>
2021-10-19 18:07:43 +0800

    類別行元件

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg'
 import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>pts02</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      total: 0, //init value
+    }
+  }
+
+  render() {
+    return (
+      <h1
+        onClick={() => {
+          this.setState({ total: this.state.total + 1 })
+        }}
+      >
+        {this.state.total}
+      </h1>
+    )
+  }
 }
 
 export default App
