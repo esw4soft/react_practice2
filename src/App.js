8b4d92907b2068b00ee65fc8823cd638c8da841e
easonw <es250528@gmail.com>
2021-10-19 18:12:56 +0800

    init03

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>12314master56</p>
+        <p>12314master03</p>
         <a
           className="App-link"
           href="https://reactjs.org"
