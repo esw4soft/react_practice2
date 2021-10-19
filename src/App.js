46ce67b58f9099774e10d57785a4a62bc22beda6
easonw <es250528@gmail.com>
2021-10-19 17:42:55 +0800

    還原

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg'
 import './App.css'
-import React, { useState } from 'react'
 
 function App() {
-  const [total, setTotal] = useState(0)
-
-  return <h1 onClick={() => setTotal(total + 1)}>{total}</h1>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
