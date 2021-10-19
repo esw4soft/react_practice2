b8a61a4e68fa1e843c289afbbb18c5a161e91ca2
easonw <es250528@gmail.com>
2021-10-19 17:47:35 +0800

    pts02

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>1231456</p>
+        <p>pts02</p>
         <a
           className="App-link"
           href="https://reactjs.org"
