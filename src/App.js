c9d17d522ed0577285ee04a093de0840955e0064
easonw <es250528@gmail.com>
2021-10-20 14:22:12 +0800

    類別型 只能有一個root tag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg'
 import './App.css'
 import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>12314master56bbbb</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      total: 0,
+      total2: 0,
+      total3: 0,
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1
+          onClick={() => {
+            this.setState({
+              total: this.state.total + 1,
+              total2: this.state.total2 + 2,
+              total3: this.state.total3 + 5,
+            })
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+          {this.state.total}
+        </h1>
+        <h1>{this.state.total2}</h1>
+        <h1>{this.state.total3}</h1>
+      </>
+    )
+  }
 }
 
 export default App
