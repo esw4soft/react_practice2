5bbe18380101dbaeae1880269ee0fab5b0923756
easonw <es250528@gmail.com>
2021-10-21 16:29:30 +0800

    Revert "testt"
    This reverts commit 8e882a383e318873b44c4e3dd33f32bb8d823b07.

--- src/components/AppClass.js
+++ src/components/AppClass.js
@@ -16,7 +16,7 @@ class AppClass extends React.Component {
         <h1
           onClick={() => {
             this.setState({
-              total: this.state.total + 1,
+              total: this.state.total + 2,
             })
           }}
         >
