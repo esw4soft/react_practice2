50fc1e43142166daec45d59af1fb800d9a134d9a
esmacbook <es250528@gmail.com>
2021-11-01 22:35:25 +0800

    input每斤新台幣轉換

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react'
 
 function Converter(props) {
   // 用於輸入使用的狀態
-  const [inputValue, setInputValue] = useState('')
-
-  console.log(inputValue)
+  const [twd, setTwd] = useState(0)
+  const [usd, setUsd] = useState(0)
 
   return (
     <>
       {/* 值（value）對應狀態值（inputValue）,更動方法（onchange）對樣狀態設定方法（setInputValue） */}
+      新台幣：
       <input
         type="text"
-        value={inputValue}
+        value={twd}
         onChange={(event) => {
-          setInputValue(event.target.value)
+          setTwd(event.target.value)
+        }}
+      />
+      <hr />
+      美金：
+      <input
+        type="text"
+        value={usd}
+        onChange={(event) => {
+          setUsd(event.target.value)
         }}
       />
     </>
