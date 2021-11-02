dbfd9bdf139de3bbbd269d9114c2dca62a3c76fe
esmacbook <es250528@gmail.com>
2021-11-02 22:22:24 +0800

    互轉單元件

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <Converter />
-      <div>1</div>
     </>
   )
 }
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -5,6 +5,12 @@ function Converter(props) {
   const [twd, setTwd] = useState(0)
   const [usd, setUsd] = useState(0)
 
+  // 美金轉新台幣
+  const usd2twd = (usd) => (28.53 * usd).toFixed(0)
+
+  // 新台幣轉美金(2位小數點)
+  const twd2usd = (twd) => (twd / 28.53).toFixed(2)
+
   return (
     <>
       {/* 值（value）對應狀態值（inputValue）,更動方法（onchange）對樣狀態設定方法（setInputValue） */}
@@ -13,7 +19,14 @@ function Converter(props) {
         type="text"
         value={twd}
         onChange={(event) => {
-          setTwd(event.target.value)
+          // 先得到要變動的直 注意網頁上輸入的是字串需要轉為數字類型
+          const newValue = +event.target.value
+
+          // 設定新台幣值
+          setTwd(newValue)
+
+          // 設定轉換後的美金
+          setUsd(twd2usd(newValue))
         }}
       />
       <hr />
@@ -22,7 +35,9 @@ function Converter(props) {
         type="text"
         value={usd}
         onChange={(event) => {
-          setUsd(event.target.value)
+          const newValue = +event.target.value
+          setUsd(newValue)
+          setTwd(usd2twd(newValue))
         }}
       />
     </>
