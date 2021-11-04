79c6d739ce58fac8ffdee3fb09d133b581f1579a
esmacbook <es250528@gmail.com>
2021-11-04 22:13:51 +0800

    下拉選單map+使用usestatet儲存渲染

--- src/components/ZipCode.js
+++ src/components/ZipCode.js
@@ -7,11 +7,25 @@ function ZipCode() {
   console.log(postcodes)
 
   //   記錄資料陣列的索引職
-  // const [country, setCountry] =c
+  const [country, setCountry] = useState(-1)
 
   return (
     <>
       <h1>台灣郵遞區號連動下拉式選單元素</h1>
+      <select
+        value={country}
+        onChange={(event) => {
+          // 設定要轉換為數字
+          setCountry(+event.target.value)
+        }}
+      >
+        {/* 預設值為-1 */}
+        <option value={-1}>請選擇縣市</option>
+        {/* 從countries 提取縣市資料 索引值當作option的值 */}
+        {countries.map((v, i) => (
+          <option value={i}>{v}</option>
+        ))}
+      </select>
     </>
   )
 }
