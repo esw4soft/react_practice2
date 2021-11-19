9cf7b520c8cf883c3692f68879d2d207f35531fd
easonw <es250528@gmail.com>
2021-11-19 11:57:16 +0800

    header-content-footer-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
-import { FaReact } from 'react-icons/fa'
-import { GiArrowed } from 'react-icons/gi'
+import React from 'react'
+import MyNavbar from './components/MyNavbar'
+import MyFooter from './components/MyFooter'
+import MainContent from './components/MainContent'
 
 function App() {
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
   return (
     <>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col md="auto">
-            <h1>react bootstrap & icons</h1>
-            <GiArrowed size="50" color="blue" />
-            <Button variant="primary">primary</Button>
-            <Button variant="secondary">secondary</Button>
-            <Button variant="success">success</Button>
-            <Button variant="primary" onClick={handleShow}>
-              <FaReact />
-              lauch static backdrop modal
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>i will not clase id you click outside me</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            close
-          </Button>
-          <Button variant="primary">understood</Button>
-        </Modal.Footer>
-      </Modal>
+      <MyNavbar />
+      <MainContent>
+        <h1 className="mt-5">Sticky footer with fixed navbar</h1>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+        <p className="lead">
+          Pin a footer to the bottom of the viewport in desktop browsers with
+          this custom HTML and CSS. A fixed navbar has been added with{' '}
+          <code>padding-top: 60px;</code> on the{' '}
+          <code>main &gt; .container</code>.
+        </p>
+      </MainContent>
+      <MyFooter />
     </>
   )
 }
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  NavDropdown,
+} from 'react-bootstrap'
+
+function MyNavbar(props) {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="primary"
+        variant="dark"
+        fixed="top"
+      >
+        <Navbar.Brand href="#home">react-bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">fratures</Nav.Link>
+            <Nav.Link href="#pricing">pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">more deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  )
+}
+
+export default MyNavbar
