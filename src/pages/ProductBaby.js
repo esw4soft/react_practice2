b5629408f3d80afd5bbfc8c2f0f6ddb2075bf9a5
easonw <es250528@gmail.com>
2021-11-23 12:16:59 +0800

    withRouter(HOC)

--- src/pages/ProductBaby.js
+++ src/pages/ProductBaby.js
@@ -1,7 +1,11 @@
 import React from 'react'
+// 高階元件樣式(HOC) 增強元件用的
+import { withRouter } from 'react-router-dom'
 
 function ProductBaby(props) {
+  // 需經過高階元件後才能得到react-router的三大鼠性質
+  console.log(props)
   return <>produect -baby</>
 }
 
-export default ProductBaby
+export default withRouter(ProductBaby)
