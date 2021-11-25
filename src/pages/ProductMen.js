0e3ff383634cf0acd983fd750b262965c5be1173
easonw <es250528@gmail.com>
2021-11-25 13:44:59 +0800

    查詢網址參數

--- src/pages/ProductMen.js
+++ src/pages/ProductMen.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
+// 高階元件樣式(HOC) 增強元件用
+import { withRouter } from 'react-router-dom'
+
 function ProductMen(props) {
-  return <>produect - men</>
+  console.log(props)
+
+  let searchParams = new URLSearchParams(
+    props.location.search
+  )
+
+  // iterate the search parameters
+  for (let p of searchParams) {
+    console.log(p)
+  }
+
+  return (
+    <>
+      <h1>product - men</h1>
+      <p>動作(action) : {searchParams.get('action')}</p>
+      <p>
+        查詢字串(keywords) : {searchParams.get('keywords')}
+      </p>
+    </>
+  )
 }
 
-export default ProductMen
+export default withRouter(ProductMen)
