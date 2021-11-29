850ca8a0dc09dd5e6ba9bac7d02744b0372c3d49
easonw <es250528@gmail.com>
2021-11-29 18:30:15 +0800

    會員get未完成

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Member(props) {
+  const [members, setMembers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function getMembers() {
+    // 開始仔入資料 先出現spinner
+    setIsLoading(true)
+
+    // 要使用try-catch來作錯誤處理
+    try{
+      // 從伺服器得到資料
+    }
+  }
   return (
     <>
       <h1>會員管理</h1>
