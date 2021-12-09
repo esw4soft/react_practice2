531f6db5fed06573d266b647775bffd2e252fd2d
easonw <es250528@gmail.com>
2021-12-09 11:57:23 +0800

    使用react-thunk+settimeout模擬異步

--- src/pages/CounterRedux.js
+++ src/pages/CounterRedux.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 // import {
 //   ADD_VALUE,
 //   SUB_VALUE,
 // } from '../actions/actionTypes'
 
-import { addValue, subValue } from '../actions'
+import {
+  addValue,
+  subValue,
+  addValueAsync,
+} from '../actions'
 
 function CounterRedux(props) {
   // 觀察props裡的得到store對應和方法
   console.log(props)
   console.log(props.total)
+
+  // 初始async
+  // useEffect(() => {
+  //   props.initValueAsync()
+  // }, [])
 
   return (
     <>
@@ -28,6 +37,14 @@ function CounterRedux(props) {
         }}
       >
         +1
+      </button>
+      <button
+        onClick={() => {
+          // async操作
+          props.addValueAsync(1)
+        }}
+      >
+        +1(3秒後-模擬異步)
       </button>
       <button
         onClick={() => {
@@ -78,7 +95,7 @@ function CounterRedux(props) {
 // 將redux中的store的state(狀態)
 // 對應這個元件中的props中 名稱為total
 const mapStateToProps = (store) => {
-  return { total: store.todos }
+  return { total: store.counter.count }
 }
 
 // 不使用這個值 略過後自動綁定store的dispatch方法到這個元件的props
@@ -89,4 +106,5 @@ const mapStateToProps = (store) => {
 export default connect(mapStateToProps, {
   addValue,
   subValue,
+  addValueAsync,
 })(CounterRedux)
