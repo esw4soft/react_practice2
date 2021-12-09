0d322e58d95a3fcd3ec0575d96628d33d5ef54e1
easonw <es250528@gmail.com>
2021-12-09 14:04:51 +0800

    redux-thunk連接伺服器get

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,21 +7,30 @@ import {
   INIT_VALUE,
 } from './actionTypes'
 // 初始化動作物件建立器
-// export const initValue = (value) => {
-//   return { type: INIT_VALUE, value }
-// }
+export const initValue = (value) => {
+  return { type: INIT_VALUE, value }
+}
 
-// // 從伺服器一開始要載入資料用
-// export const initValueAsync = (value) => {
+// 從伺服器一開始要載入資料用
+export const initValueAsync = (value) => {
+  return async (dispatch) => {
+    const url = ''
 
-// }
+    const response = await fetch(url, { method: 'GET' })
+    const data = await response.json()
 
-// 一步-3秒後會加值
+    console.log(data)
+
+    // 得到資料後發送初始化動作
+    dispatch(initValue(data.total))
+  }
+}
 
 export const addValue = (value) => {
   return { type: ADD_VALUE, value }
 }
 
+// 異步-3秒後會加值
 export const addValueAsync = (value) => {
   return (dispatch) => {
     setTimeout(() => {
